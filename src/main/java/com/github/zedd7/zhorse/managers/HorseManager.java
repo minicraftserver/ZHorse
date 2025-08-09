a1ab37371bf02de8dfe23176358e50c4748decbc
Schneidertm <103960641+Schneidertm@users.noreply.github.com>
2025-08-09 18:14:33 +0200

    Update to Paper 1.21.8 and fix attribute names

--- src/main/java/com/github/zedd7/zhorse/managers/HorseManager.java
+++ src/main/java/com/github/zedd7/zhorse/managers/HorseManager.java
@@ -262,14 +262,14 @@ public class HorseManager {
 		if (statsRecord.getDomestication() != null) horse.setDomestication(statsRecord.getDomestication());
 		if (statsRecord.getFireTicks() != null) horse.setFireTicks(statsRecord.getFireTicks());
 		if (statsRecord.isGlowing() != null) horse.setGlowing(statsRecord.isGlowing());
-		if (statsRecord.getMaxHealth() != null) horse.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(statsRecord.getMaxHealth());
+if (statsRecord.getMaxHealth() != null) horse.getAttribute(Attribute.MAX_HEALTH).setBaseValue(statsRecord.getMaxHealth());
 		if (statsRecord.getHealth() != null) horse.setHealth(statsRecord.getHealth()); // Define maxHealt before current health
 		if (statsRecord.getJumpStrength() != null) horse.setJumpStrength(statsRecord.getJumpStrength());
 		if (statsRecord.getNoDamageTicks() != null) horse.setNoDamageTicks(statsRecord.getNoDamageTicks());
 		if (statsRecord.getRemainingAir() != null) horse.setRemainingAir(statsRecord.getRemainingAir());
 		if (statsRecord.isTamed() != null) horse.setTamed(statsRecord.isTamed());
 		if (statsRecord.getTicksLived() != null) horse.setTicksLived(statsRecord.getTicksLived());
-		if (statsRecord.getSpeed() != null) horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(statsRecord.getSpeed());
+if (statsRecord.getSpeed() != null) horse.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(statsRecord.getSpeed());
 		if (statsRecord.getType() != null) {
 			if (statsRecord.getType().equals(EntityType.HORSE.name())) {
 				if (statsRecord.getColor() != null) ((Horse) horse).setColor(Horse.Color.valueOf(statsRecord.getColor()));
